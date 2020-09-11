f9fefdb0e2d63b6a74c19346b4df32327f213854
Aslak Frafjord Skailand <aslakfrafjord.skailand@altibox.no>
2020-09-11 15:47:06 +0200

    added assignment 1 a

---
+++ assignment_1/assignment_1.py
@@ -0,0 +1,54 @@
+hand = ['ROCK', 'PAPER', 'SCISSORS']
+
+def RoPaSc(playerOneInput, playerTwoInput):
+    if (playerTwoInput == playerOneInput):
+        choise = input('Its a tie! Play again? (y/n) \n  >')
+        playAgain(choise)
+    elif(playerOneInput == 'ROCK'):
+        if(playerTwoInput == 'PAPER'):
+            playerWin(2)
+        if(playerTwoInput == 'SCISSORS'):
+            playerWin(1)
+    elif(playerOneInput == 'PAPER'):
+        if(playerTwoInput == 'ROCK'):
+            playerWin(1)
+        if(playerTwoInput == 'SCISSORS'):
+            playerWin(2)
+    elif(playerOneInput == 'SCISSORS'):
+        if(playerTwoInput == 'ROCK'):
+            playerWin(2)
+        if(playerTwoInput == 'PAPER'):
+            playerWin(1)
+        
+def playerWin(player):
+    print('Player ' + str(player) + ' wins!')
+    choise = input('Its a tie! Play again? (y/n) \n  >')
+    playAgain(choise)
+
+def getPlayerInput(player):
+    playerInput = input(player + ': Rock, papers or scissors? type q to quit \n  > ').upper()
+    if (not checkInput(playerInput)):
+        print('* ' + playerInput + ' *' + ' is not a valid input... Try again!')
+        getPlayerInput(player)
+    return playerInput
+    
+def checkInput(playerInput):
+    if playerInput == 'Q':
+        exit()
+    return playerInput in hand
+
+def playAgain(choise):
+    if choise == 'y':
+        playerOneInput = getPlayerInput('Player 1')
+        playerTwoInput = getPlayerInput('Player 2')
+        RoPaSc(playerOneInput, playerTwoInput)
+    elif choise == 'n':
+        exit()
+    else:
+        playAgain(input('Invalid input!'))
+    
+
+if __name__ == "__main__":
+    playerOneInput = getPlayerInput('Player 1')
+    playerTwoInput = getPlayerInput('Player 2')
+    RoPaSc(playerOneInput, playerTwoInput)
